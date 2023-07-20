34c3c67152655768f04e9a77f69b39cd9bc62a2e
usmankhan <uka3304@gmail.com>
2023-07-21 02:49:41 +0500

    made some changes in the url pattern

--- emp_app/urls.py
+++ emp_app/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('view_emp',views.view_emp, name="view_emp"),
     path('add_emp',views.add_emp, name="add_emp"),
     path('remove_emp/',views.remove_emp, name="remove_emp"),
-    path('filter_emp/',views.filter_emp, name="filter_emp"),
+    path('filter_emp',views.filter_emp, name="filter_emp"),
     path('remove_emp<int:id>',views.remove_emp,name="remove_emp")
 
 
